4c9fa876aef936ee9022c58f29f5306b5aeff4f5
Laurent Lienher <laurent.lienher@gmail.com>
2015-03-16 09:32:37 +0100

    fix

--- www/js/issues.js
+++ www/js/issues.js
@@ -1,7 +1,12 @@
 angular.module('citizen-engagement.issues', ['angular-storage'])
 
-.factory('issueService', ['issueService', function(){
-	return function name(){
-		
-	};
+.factory('issueService', ['issueService', 
+	function($http){
+		function getIssues (){
+			$http.get(apiUrl + 'users').sucess().error();			
+		}
+
+		function addIssue (){
+			$http.post(apiUrl + 'users').sucess().error();
+		}
 }]);

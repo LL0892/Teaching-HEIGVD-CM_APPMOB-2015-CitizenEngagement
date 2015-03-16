cdb3669055b56602f44d96332b5cea138ad5b23b
Laurent Lienher <laurent.lienher@gmail.com>
2015-03-16 14:24:34 +0100

    issues and issuetypes service

--- www/js/issues.js
+++ www/js/issues.js
@@ -11,28 +11,107 @@ angular.module('citizen-engagement.issues', ['angular-storage'])
 			}
 		)
 
-		/*$scope.addIssue = function(){
+
+		addIssue = function (callback, errorCallback){
 			$http({
 				method: 'POST',
-				url: apiUrl + 'issues',
-				data: $scope.issue
-			}).success(function()
-		}*/
+				url: apiUrl + '/issues/',
+				data: issue
+			}).success(function(data, status, headers, config){
+				$scope.issue = data;
+				$log.debug(status);
+			}).error(function(data, status, headers, config){
+				errorCallback(data);
+			})		
+		};
+
+
 })
 
 .factory('IssueService', function($http, apiUrl, $log){
 	return {
+
+		// ********
+		// ISSUES
+		// ********
+
+		// Get a list of issues.
 		getIssues : function (callback, errorCallback){
 			$http({
 				method: 'GET',
-				url: apiUrl + '/issues/',
+				url: apiUrl + '/issues/'
 			}).success(function(data, status, headers, config){
 				callback(data);
 				$log.debug(status);
 			}).error(function(data, status, headers, config){
 				errorCallback(data);
 			});		
-		}
+		},
 
+		// Create a new issue.
+		postIssue : function (callback, errorCallback){
+			$http({
+				method: 'POST',
+				url: apiUrl + '/issues',
+				data: issue
+			}).success(function(data, status, headers, config){
+				// todo			
+			}).error(function(data, status, headers, config){
+				// todo
+			});
+		},
+
+		// Retrieve the details of a specific issue.
+		getIssueDetail : function(callback, errorCallback){
+			$http({
+				method: 'GET',
+				url: apiUrl + '/issues/:id'
+				//data: issueId
+			}).success(function(data, status, headers, config){
+				// todo
+			}).error(function(data, status, headers, config){
+				// todo
+			});
+		},
+
+		// Perform MongoDB queries directly by given the query through the JSON payload. 
+		//Refers to the MongoDB query. Queries are limited to the issues only.
+		searchIssue : function(callback, errorCallback){
+
+		},
+
+		// Perform an action on the issue. (available for users)
+		// 2 types of action : comments, tags
+		actionIssue : function(callback, errorCallback){
+
+		},
+
+		// ************
+		// ISSUE TYPES
+		// ************
+
+		// Retrieve the list of issue types.
+		getIssueTypes : function(){
+			$http({
+				method: 'GET',
+				url: apiUrl + '/issuetypes'
+			}).success(function(data, status, headers, config){
+				// todo
+			}).error(function(data, status, headers, config){
+				// todo
+			});
+		},
+
+		// Retrieve the details of a specific issue type.
+		getIssueTypeDetail : function(){
+			$http({
+				method: 'GET',
+				url: apiUrl + '/issuetypes/:id'
+			}).success(function(data, status, headers, config){
+				// todo
+			}).error(function(data, status, headers, config){
+				// todo
+			});			
+		}
 	}
 });
